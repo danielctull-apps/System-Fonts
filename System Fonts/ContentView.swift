98f48975e2dd7b01e334ceceb96ecf994dd15347
Daniel Tull <dt@danieltull.co.uk>
2019-10-18 15:01:33 +0100

    Conform UIFont.TextStyle to CaseIterable

--- System Fonts/ContentView.swift
+++ System Fonts/ContentView.swift
@@ -19,23 +19,14 @@ struct DesignSection: View {
 
     let design: UIFontDescriptor.SystemDesign
 
+    private func cell(for style: UIFont.TextStyle) -> some View {
+        Text(style.description)
+            .font(.preferredFont(forTextStyle: style, design: design))
+    }
+
     var body: some View {
         Section(header: Text(design.description).font(.preferredFont(forTextStyle: .headline, design: design))) {
-            Group {
-                Text("largeTitle").font(.preferredFont(forTextStyle: .largeTitle, design: design))
-                Text("title1").font(.preferredFont(forTextStyle: .title1, design: design))
-                Text("title2").font(.preferredFont(forTextStyle: .title2, design: design))
-                Text("title3").font(.preferredFont(forTextStyle: .title3, design: design))
-                Text("headline").font(.preferredFont(forTextStyle: .headline, design: design))
-            }
-            Group {
-                Text("subheadline").font(.preferredFont(forTextStyle: .subheadline, design: design))
-                Text("body").font(.preferredFont(forTextStyle: .body, design: design))
-                Text("callout").font(.preferredFont(forTextStyle: .callout, design: design))
-                Text("footnote").font(.preferredFont(forTextStyle: .footnote, design: design))
-                Text("caption1").font(.preferredFont(forTextStyle: .caption1, design: design))
-                Text("caption2").font(.preferredFont(forTextStyle: .caption2, design: design))
-            }
+            ForEach(UIFont.TextStyle.allCases, id: \.self, content: cell)
         }
     }
 }
---
+++ System Fonts/UIFont.TextStyle.swift
@@ -0,0 +1,41 @@
+
+import UIKit
+
+extension UIFont.TextStyle: CustomStringConvertible {
+
+    public var description: String {
+        switch self {
+        case .largeTitle: return "largeTitle"
+        case .title1: return "title1"
+        case .title2: return "title2"
+        case .title3: return "title3"
+        case .headline: return "headline"
+        case .subheadline: return "subheadline"
+        case .body: return "body"
+        case .callout: return "callout"
+        case .footnote: return "footnote"
+        case .caption1: return "caption1"
+        case .caption2: return "caption2"
+        default: fatalError()
+        }
+    }
+}
+
+extension UIFont.TextStyle: CaseIterable {
+
+    public static var allCases: [UIFont.TextStyle] {
+        [
+            .largeTitle,
+            .title1,
+            .title2,
+            .title3,
+            .headline,
+            .subheadline,
+            .body,
+            .callout,
+            .footnote,
+            .caption1,
+            .caption2
+        ]
+    }
+}
