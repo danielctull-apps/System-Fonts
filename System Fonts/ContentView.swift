a432fc484c85df8c58fd69892731ab88734b780a
Daniel Tull <dt@danieltull.co.uk>
2019-10-18 14:43:36 +0100

    Initial Commit

---
+++ System Fonts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  System Fonts
+//
+//  Created by Daniel Tull on 18/10/2019.
+//  Copyright © 2019 Daniel Tull. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
